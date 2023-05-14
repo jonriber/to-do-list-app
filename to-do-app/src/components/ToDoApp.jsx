7f96dc3a6b02c042199cc92e8417df3308f2eb2e
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-15 00:36:47 +0100

    structuring components

---
+++ to-do-app/src/components/ToDoApp.jsx
@@ -0,0 +1,51 @@
+import {useReducer, useState} from "react"
+import AddTask from "./AddTask"
+import TaskList from "./TaskList"
+import initialTasks from "../reducers/initialTasks"
+import tasksReducer from "../reducers/TasksReducer"
+
+const ToDoApp = () =>{
+    const [nextId,setNextId] = useState(3);
+    const [tasks,dispatch] = useReducer(
+        tasksReducer,
+        initialTasks
+    );
+
+    console.log("TASKS:",tasks);
+    
+    function handlerAddTask(e,text){
+        e.preventDefault();
+        console.log("ADD NEW TASK HANDLER!!!");
+        setNextId((nextId) => nextId +1);
+        dispatch({
+            type: 'added',
+            id: nextId,
+            text: text,
+        })
+    }
+
+    function handlerChangeTask(task){
+        dispatch({
+            type: "changed",
+            task: task
+        })
+    }
+
+    function handlerDeleteTask(id){
+        dispatch({
+            type: "deleted",
+            id:id
+        })
+    }
+
+    return(
+        <>
+            <h3>Task App Example</h3>
+            <AddTask addTaskHandler={handlerAddTask}/>
+            <TaskList tasks={tasks} changeTaskHandler={handlerChangeTask} deleteTaskHandler={handlerDeleteTask}/>
+        </>
+    )
+}
+
+export default ToDoApp
+
